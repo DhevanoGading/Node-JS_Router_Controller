bd6a6942d5c65eb402a5417c767b98e9f3380057
DhevanoGading <88045112+DhevanoGading@users.noreply.github.com>
2022-02-15 12:30:30 +0700

    Add files via upload

---
+++ controllers/penduduk.controller.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const db = require('../db')
+
+module.exports ={
+    index: (req, res) => {
+        const sql = `SELECT * FROM data_penduduk`
+        db.query(sql, (err,result)=>{
+            if (err) throw (err)
+            res.json({
+                message: "berhasil",
+                data: result
+            })
+        })
+    },
+    add: (req, res) => {
+        let data = {
+            nama: req.body.nama,
+            alamat: req.body.alamat
+        }
+        let sql = "insert into data_penduduk SET ?";
+        if(data.alamat && data.nama){
+            db.query(sql, data, (err) => {
+                if (err){
+                    throw err
+                }else{
+                    res.json({
+                        message: "Added succes",
+                        data
+                    })
+                }
+            })
+        }
+    },
+    put: (req, res) => {
+        let id = req.body.id;
+        let updateData = {
+            nama: req.body.nama,
+            alamat: req.body.alamat
+        }
+
+        const sql = `UPDATE data_penduduk SET ? WHERE id = ?`;
+        db.query(sql, [updateData, id], (err, result) => {
+            if(err){
+                throw err
+            }else{
+                res.json({
+                    message: `Successfully update data where id = ${id}`,
+                    nama: updateData.nama,
+                    alamat: updateData.alamat
+                })
+            }
+        })
+    },
+    delete: (req, res) => {
+        let id = req.params.id;
+
+        let dataDeleted;
+
+        if(id){
+            let sql = `SELECT * FROM data_penduduk WHERE id = ?`
+            db.query(sql, id, (err,result) => {
+                if(err){
+                    throw err
+                }else{
+                    dataDeleted = result
+                }
+            })
+        }
+        if(id){
+            let sql = `DELETE FROM data_penduduk WHERE id = ?`;
+            db.query(sql, id, (err,result) => {
+                if(err){
+                    throw err
+                }else{
+                    res.json({
+                        message: `Successfully delete ID = ${id}`,
+                        deleted: dataDeleted
+                    })
+                }
+            })
+        }
+    }
+}
